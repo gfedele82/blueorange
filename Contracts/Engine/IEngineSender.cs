3674ba3da935b286ad092ef4ee376d9d0278ba9c
gfedele82 <32782417+gfedele82@users.noreply.github.com>
2022-07-26 16:41:23 -0300

    Add files via upload

---
+++ Contracts/Engine/IEngineSender.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+using System.Threading.Tasks;
+
+namespace Contracts.Engine
+{
+    public interface IEngineSender
+    {
+        Task<EmailResponse> ProcessSender(Email email);
+    }
+}
